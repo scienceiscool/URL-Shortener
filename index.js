a07118c2a0a6942f48a539f41584ff3f517c6555
Kathy <kathysaad@gmail.com>
2015-04-08 17:01:59 -0700

    Got a simple page running on localhost:3000/ where there is a textbox to enter a URL in

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 // Reference:
 // - https://scotch.io/tutorials/build-a-restful-api-using-node-and-express-4
+// - Project 1
 
 "use strict";
 
 var express = require('express');
 var app = express();
 
+// inspiration for the following from:
+// http://stackoverflow.com/questions/12038128/nodejs-using-redis-connect-redis-with-express
+// and
+// http://www.tutorialindustry.com/nodejs-redis-tutorial-for-beginners
+var http = require('http');
 var redis = require('redis');
+var client = redis.createClient();
 
 var port = process.env.PORT || 3000;
 
+// inspiration for the following from:
+// http://codeforgeek.com/2014/06/express-nodejs-tutorial/
+app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
-app.set('views', path.join(__dirname, 'views'));
 //app.engine('html', require('jade').renderFile);
 
 app.listen(port);
 console.log('Magic happening on port ' + port);
+
+app.get("/", function (req, res) {
+  return res.render("index.jade");
+});
